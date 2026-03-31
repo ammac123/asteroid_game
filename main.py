2e5b987821e6ed682f806323653584176c9e9b08
Andrew Macleod <andrew.munro.macleod@gmail.com>
2026-03-31 23:05:13 +1100

    Setup game ticks

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import pygame
-import constants
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 def main():
-    print(f"Starting Asteroids with pygame version {pygame.version.ver}")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
-    
     pygame.init()
+    
+    clock = pygame.time.Clock()
+    dt = 0
 
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         log_state()
@@ -19,10 +18,11 @@ def main():
                 return
             pass
 
-        screen.fill("black")
 
+        screen.fill("black")
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
